5a7a7726d14ae8cf31d3395eea9b37854b6ec130
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-07-13 17:33:41 +0100

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./swagger.yaml",
+    url: "./hng11-task-three-group-big-5.openapi.yaml",
     dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
